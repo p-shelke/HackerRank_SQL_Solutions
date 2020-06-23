c2a534e7c9540e59ea159b9af42bdbc01ae61806
Prathmesh Shelke <prathamesh2526@gmail.com>
2020-06-23 16:11:10 -0400

    Create revising_the_select_query_I.sql

---
+++ Basic Select/revising_the_select_query_I.sql
@@ -0,0 +1,9 @@
+Revising the Select Query I
+https://www.hackerrank.com/challenges/revising-the-select-query/problem
+
+
+Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
+
+Solution:
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000
