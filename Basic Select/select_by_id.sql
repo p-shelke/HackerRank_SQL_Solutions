89914099ed1f97aacc6b8563a6a93fe8f8330cf1
Prathmesh Shelke <prathamesh2526@gmail.com>
2020-06-24 01:30:32 -0400

    Select By ID

---
+++ Basic Select/select_by_id.sql
@@ -0,0 +1,7 @@
+Select By ID
+https://www.hackerrank.com/challenges/select-by-id/problem?h_r=next-challenge&h_v=zen
+
+Query all columns for a city in CITY with the ID 1661.
+
+Solution:
+SELECT * FROM CITY WHERE ID = 1661 ;
